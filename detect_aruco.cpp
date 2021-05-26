78b050d9b9eca0377ba0f20a435fb1a3b4edeeea
linbreux <29354411+Linbreux@users.noreply.github.com>
2021-05-26 22:58:30 +0200

    Commentaar toevoegen ter verduidelijking

--- detect_aruco.cpp
+++ detect_aruco.cpp
@@ -8,12 +8,14 @@
 #include "pid.hpp"
 #include <iostream>
 
+//grootte van de referentiemarker in CM
 float X_VAN_REFMARK = 2.1;
 float Y_VAN_REFMARK = 2.1;
 
 float PIX_PER_CM_X;
 float PIX_PER_CM_Y;
 
+//hoek waar die de detectiemarkers dienen aan te namen.
 int gewensteHoek = 180;
 
 Point2f testSetP(-1,-1);
@@ -25,20 +27,21 @@ float HOEK_REF;
 using namespace cv;
 using namespace std;
 
-// geef een punt mee uit het camera beeld dat rond het referentiepunt zal roteren.
+// geef een punt mee uit het camera beeld dat rond het referentiepunt zal roteren. Camerabeeld --> referentiemarker
 Point2f rotMatrixRef(Point2f p){
     float x = REF_PUNT.x+(p.x-REF_PUNT.x)*cos(HOEK_REF)-(p.y-REF_PUNT.y)*sin(HOEK_REF);
     float y = REF_PUNT.y+(p.x-REF_PUNT.x)*sin(HOEK_REF)+(p.y-REF_PUNT.y)*cos(HOEK_REF);
     return Point2f(x,y);
 }
 
+//as referentiemarker --> camerabeeld
 Point2f invRotMatrixRef(Point2f p){
     float x = REF_PUNT.x+(p.x-REF_PUNT.x)*cos(HOEK_REF)+(p.y-REF_PUNT.y)*sin(HOEK_REF);
     float y = REF_PUNT.y-(p.x-REF_PUNT.x)*sin(HOEK_REF)+(p.y-REF_PUNT.y)*cos(HOEK_REF);
     return Point2f(x,y);
 }
 
-
+// sla pixelwaarde van de muisklik op in testSetP
 void muis_click(int event,int x,int y,int flags,void *param){
     if (event == EVENT_LBUTTONDOWN){
         cout << "x: " <<x << " y: " <<y <<endl;
@@ -61,32 +64,41 @@ int main(){
     // video naar frame 
     v >> frame;
     Mat outputFrame;
-    
+	
+    // PIDs aanmaken
     PID pidx = PID(0.033,100,-100,0.5,0.01,0);
     PID pidy = PID(0.033, 100,-100,0.5,0.01,0);
     //PID pidx = PID(0.033,100,-100,0,0.0,0);
     //PID pidy = PID(0.033, 100,-100,0.0,0.0,0);
     PID pidz = PID(0.033, 100,-100,0.5,0.01,0.5);
 
+    // start loop als er een frame beschikbaar is
     while(v.grab()){
+	// laad laatste frame
         v >> frame;
         outputFrame =frame.clone();
 
+	// laad aruco bib
         Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_6X6_250);
         
+	// maak lijst waar ids en hoeken van de gedetecteerde markers worden opgeslaan.
         std::vector<int> ids;
         std::vector<std::vector<cv::Point2f>> corners;
+	    
+	// detecteer de markers en omtrek deze
         aruco::detectMarkers(frame,dictionary,corners,ids);
         aruco::drawDetectedMarkers(outputFrame,corners,ids);
 
-
-        for (int i =0;i<corners.size(); i++){
+	// Voor elke marker doe...
+        for (int i =0;i<corners.size(); i++){	
+	    // indien detextiemarker (id=1)
             if (ids[i] == 1){
                 // Midpunt van de ref marker bepalen
                 Lijn schuin(corners[i][0], corners[i][2]);             
                 circle(outputFrame,schuin.getMidPunt(),5,Scalar(0,0,255),4);
                 REF_PUNT = schuin.getMidPunt();
-
+		
+		// zet setpunt op detectiewaarde
                 if (testSetP.x == -1){
                     testSetP = REF_PUNT;
                 }
@@ -98,27 +110,34 @@ int main(){
                 // lijn links
                 Lijn links(corners[i][0],corners[i][1]);
 
+		// bepaal aantal pixels per CM
                 PIX_PER_CM_X = onder.getLengte()/X_VAN_REFMARK;
                 PIX_PER_CM_Y = links.getLengte()/Y_VAN_REFMARK;
 
                 //putText(outputFrame,to_string(onder.getHoekTovXAs()),onder.getP2(),1,2,Scalar(0,255,0));
-
+		    
+		//teken as van 10cm
                 float x_as = REF_PUNT.x + 10*PIX_PER_CM_X;
                 float y_as = REF_PUNT.y + 10*PIX_PER_CM_Y;
 
                 //testSetP = Point2f(REF_PUNT.x+50,REF_PUNT.y+50);
 
                 //circle(outputFrame,rotMatrixRef(testSetP),5,Scalar(0,255,0),5);
+		
+		// roteer de x-en y-as naar het referentieassenstelsel
 
                 xAsRot = rotMatrixRef(Point2f(x_as,REF_PUNT.y));
                 yAsRot = rotMatrixRef(Point2f(REF_PUNT.x,y_as));
                 //cout << "xRef: " << PIX_PER_CM_Y;
 
+		// teken de punten van het assenstelsel
                 line(outputFrame,REF_PUNT, xAsRot, Scalar(0,255,255),2);
                 line(outputFrame,REF_PUNT, yAsRot, Scalar(0,255,255),2);
                 
             }
+		// Bij elke andere gedetecteerde marker (id != 1)
             else{
+		//bepaal hoekpunten, middelpunten en rotatie.
                 Lijn detectieMarker(corners[i][0], corners[i][2]);             
                 circle(outputFrame,detectieMarker.getMidPunt(),5,Scalar(0,255,255),4);
 
@@ -140,6 +159,8 @@ int main(){
 		//
 		//
 		cout << "detect hoek: " << detectHoek+360;
+		    
+		// bepalen van de hoekverschillen tussen de detectiehoek en de gewenste hoek telkens rekening houdend metde kortste hoek.
 		int error;
 		if(abs(gewensteHoek-detectHoek)<180){
 			error = gewensteHoek-detectHoek;
@@ -151,11 +172,13 @@ int main(){
 			}
 
 		}
+		// Bereken de stuurwaarde X, Y en Z met de vooraf ingestelde waarde 
                 double pX = pidx.calculate(sP.x, dM.x);
                 double pY = -pidy.calculate(sP.y, dM.y);
 		
 		double pZ = pidz.calculate(0,error);
 
+		// vertaal piwelwaarde naar cm
                	double xW, yW,zW;
 	       	xW= x/PIX_PER_CM_X;
 		yW = y/PIX_PER_CM_Y;
@@ -163,17 +186,19 @@ int main(){
 		setZwaartepunt(xW,yW,0.0);
 		cout << "x: " <<pX << " y: " <<pY <<endl;
 
+		// x,y en z waarde naar motorcontroller die deze zal vertalen naar motorspecifieke waarde.
                 set_robot_speed(pX,pY,pZ);
 
 
             }
+    	    // Zet motors stil wanneer geen markers of enkel de refmarker wordt gedetecteerd.
             if (ids.size() == 0 || (ids.size() == 1 && ids[0] == 1)){
                     set_motor_speed(1, 0);
                     set_motor_speed(2, 0);
                     set_motor_speed(3, 0);
             }
         }
-
+	// Callback event naar muis_click
         setMouseCallback("live", muis_click);
 
         // teken assenstelsel volgens 
